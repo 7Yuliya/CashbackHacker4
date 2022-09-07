68a1d643e69dab5e663c8c585817dfe4e2c4d01c
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-07 12:34:50 -0700

    Fixed src/

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,38 +1,48 @@
 package ru.netology.service;
 
 //import org.junit.jupiter.api.Test;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
-import static org.testng.AssertJUnit.assertEquals;
+import static org.junit.Assert.assertEquals;
+//import org.testng.annotations.Test;
+
+//import static org.testng.AssertJUnit.assertEquals;
 
 //import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CashbackHackServiceTest {
 
     @Test
-    void calculateCashback() {
+    public void calculateCashback() {
 
 
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
-        int actual = 100;
-        int expected = service.remain(amount);
+        int actual = service.remain(amount);
+        int expected = 100;
 
-
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
-    void calculateCashback800() {
+    public void calculateCashback800() {
 
         CashbackHackService service = new CashbackHackService();
         int amount = 800;
-        int actual = 200;
-        int expected = service.remain(amount);
+        int actual = service.remain(amount);
+        int expected = 200;
 
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void calculateCashback1000() {
 
-        assertEquals(actual, expected);
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
     }
 }
-
-
