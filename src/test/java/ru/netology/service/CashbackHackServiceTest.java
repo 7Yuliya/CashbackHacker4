2ba89471abc27be0909fd67a35c60fd5e329dcb2
Юлия Алексина <uliyaalexina@mail.ru>
2022-09-07 12:06:48 -0700

    Added src/

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    void calculateCashback() {
+
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = 100;
+        int expected = service.remain(amount);
+
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void calculateCashback800() {
+
+        CashbackHackService service = new CashbackHackService();
+        int amount = 800;
+        int actual = 200;
+        int expected = service.remain(amount);
+
+
+        assertEquals(actual, expected);
+    }
+}
+
+
